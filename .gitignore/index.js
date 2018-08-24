f4872429acf8fd02b0316474ffa80b05b41a7eb9
RoadRageFrance <38570453+RoadRageFrance@users.noreply.github.com>
2018-08-24 03:44:33 +0200

    Add files via upload

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -7,14 +7,30 @@ bot.on('ready', () =>  {
     console.log("Je suis connecté !")
   });
 
-bot.login(process.env.TOKEN)
+bot.login('NDgyMzQxNDMwMTI4NDEwNjQ0.DmDfIg.A8NNGbMdG65_9Do5XPVEJlNX7qc')
+
+bot.on("guildMemberAdd", member => {
+    const bvn = member.guild.channels.find(m => m.name === "accueil-messages");
+    if(!bvn) return;
+    bvn.send(`**Bienvenue sur le serveur test développement ${member}**`)
+    console.log("Join")
+})
+
+bot.on("guildMemberRemove", member => {
+    const bye = member.guild.channels.find(m => m.name === "accueil-messages");
+    if(!bye) return;
+    bye.send(`**${member} vient de nous quitter...**`)
+    console.log("Quit")
+})
 
 bot.on('message', message => {
     if(message.content.startsWith(prefix + "cc")) {
         message.channel.send("**Salut !**")
+        console.log("CC")
     }
     
     if(message.content.startsWith(prefix + "hey")) {
         message.reply("**Yo le S**")
+        console.log("hey")
     }
 })
